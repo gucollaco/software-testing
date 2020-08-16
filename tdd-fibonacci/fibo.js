f07fdb88df75ca8e647c8351217ac64c44e30094
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-16 15:59:37 -0300

    second test green stage ok

--- tdd-fibonacci/fibo.js
+++ tdd-fibonacci/fibo.js
@@ -1,4 +1,6 @@
 const fibo = (length, currentArray = [0, 1]) => {
+    if (!(typeof length === 'number' && (length % 1) === 0)) throw new Error('Parameter must be an integer')
+
     if (length === currentArray.length) return currentArray
 
     const newArray = [...currentArray, 0]
--- tdd-fibonacci/fibo.test.js
+++ tdd-fibonacci/fibo.test.js
@@ -6,7 +6,9 @@ describe('Fibonacci generator lib tests', function() {
         expect(Array.isArray(fiboArray)).toBe(true)
     })
     test('Assert the parameter given is an integer', function() {
-        const executeFibo = () => fibo('3')
-        expect(executeFibo).toThrowError(/Parameter must be an integer/)
+        const executeFiboString = () => fibo('3')
+        const executeFiboFloat = () => fibo(3.5)
+        expect(executeFiboString).toThrowError(/Parameter must be an integer/)
+        expect(executeFiboFloat).toThrowError(/Parameter must be an integer/)
     })
 })
