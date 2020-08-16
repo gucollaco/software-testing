02ff0c6a4734655832e995f433a2b4de9a135bc5
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-16 16:32:56 -0300

    green stage for fourth test ok

--- tdd-fibonacci/fibo.js
+++ tdd-fibonacci/fibo.js
@@ -4,6 +4,8 @@ const fibo = (length, currentArray = [0, 1]) => {
     if (!isInteger(length)) throw new Error('Parameter must be an integer')
 
     if (!gt(length, 0)) throw new Error('Parameter must be greater than 0')
+    
+    if (isEqual(length, 1)) return [0]
 
     if (isEqual(length, currentArray.length)) return currentArray
 
