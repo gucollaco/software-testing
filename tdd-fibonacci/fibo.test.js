4a12609bf7c1aceb9b00c9dd558d0c6a52948946
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-16 16:09:53 -0300

    third test

--- tdd-fibonacci/fibo.test.js
+++ tdd-fibonacci/fibo.test.js
@@ -11,4 +11,10 @@ describe('Fibonacci generator lib tests', function() {
         expect(executeFiboString).toThrowError(/Parameter must be an integer/)
         expect(executeFiboFloat).toThrowError(/Parameter must be an integer/)
     })
+    test('Assert the parameter given is greater than 0', function() {
+        const executeFiboZero = () => fibo(0)
+        const executeFiboNegative = () => fibo(-2)
+        expect(executeFiboZero).toThrowError(/Parameter must be greater than 0/)
+        expect(executeFiboNegative).toThrowError(/Parameter must be greater than 0/)
+    })
 })
