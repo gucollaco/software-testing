20ae0ae36f3bad3dd662e401f55049640a477958
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-29 13:26:03 -0300

    some more expects inserted

--- equivalence-classes-silly-pascal/silly-pascal.test.js
+++ equivalence-classes-silly-pascal/silly-pascal.test.js
@@ -5,9 +5,25 @@ describe('Silly pascal input validator tests', function() {
     test('Input that contains a special character', function() { expect(isValid("ab*12")).toBe(false) })
     test('Input with length equals to 0', function() { expect(isValid("")).toBe(false) })
     test('Input with null value', function() { expect(isValid(null)).toBe(false) })
-    test('Input with length greater than 6', function() { expect(isValid("aabb112233")).toBe(false) })
-    test('Input with length greater than 6, on the limit (equals to 7)', function() { expect(isValid("z777777")).toBe(false) })
+    test('Input with length greater than 6', function() {
+        const input = "aabb112233"
+        expect(input.length).toBeGreaterThan(6)
+        expect(isValid(input)).toBe(false)
+    })
+    test('Input with length greater than 6, on the limit (equals to 7)', function() {
+        const input = "z777777"
+        expect(input.length).toBe(7)
+        expect(isValid(input)).toBe(false)
+    })
     test('Input matching conditions', function() { expect(isValid("zyx76")).toBe(true) })
-    test('Input matching conditions, with length equals to 1', function() { expect(isValid("g")).toBe(true) })
-    test('Input matching conditions, with length equals to 6', function() { expect(isValid("zyc321")).toBe(true) })
+    test('Input matching conditions, with length equals to 1', function() {
+        const input = "g"
+        expect(input.length).toBe(1)
+        expect(isValid(input)).toBe(true)
+    })
+    test('Input matching conditions, with length equals to 6', function() {
+        const input = "zyc321"
+        expect(input.length).toBe(6)
+        expect(isValid(input)).toBe(true)
+    })
 })
