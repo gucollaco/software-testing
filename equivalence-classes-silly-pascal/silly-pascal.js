18ffe05bf45c08f0b63f29191e0eb3467ffdb346
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-08-29 13:18:20 -0300

    fix variable name

--- equivalence-classes-silly-pascal/silly-pascal.js
+++ equivalence-classes-silly-pascal/silly-pascal.js
@@ -1,7 +1,7 @@
 const isValid = (input) => {
     if (input === null) return false
     if (input.length < 1 || input.length > 6) return false
-    if (!(/^[a-zA-Z]+$/).test(identifier.charAt(0)) || !(/^[0-9a-zA-Z]+$/).test(identifier)) return false
+    if (!(/^[a-zA-Z]+$/).test(input.charAt(0)) || !(/^[0-9a-zA-Z]+$/).test(input)) return false
 
     return true
 }
