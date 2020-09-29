5e223643a51cee2379a1ce97926e6d5b06952686
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-09-29 02:08:42 -0300

    code organized and remaining tests created

--- tdd-bowling-kata/bowling-game.js
+++ tdd-bowling-kata/bowling-game.js
@@ -1,16 +1,49 @@
 class BowlingGame {
-    constructor(rolls=[], score=0, curRoll=0) {
+    constructor(rolls=Array.from(Array(20), () => 0), score=0, curRoll=0) {
         this.rolls = rolls
-        this.scores = score
+        this.score = score
         this.curRoll = curRoll
     }
 
-    score() {
+    getScore() {
+        let score = 0
+        let index = 0
+
+        for (let frame = 0; frame < 10; frame++) {
+          if (this.isStrike(index)) {
+            score += 10 + this.strikeBonus(index)
+            index++
+          } else if (this.isSpare(index)) {
+            score += 10 + this.spareBonus(index)
+            index += 2
+          } else {
+            score += this.frameSum(index)
+            index += 2
+          }
+        }
+
+        return score
     }
 
-    roll(pins) {
-        this.score += pins
-        this.rolls[this.curRoll++] = pins
+    roll(pins) { this.rolls[this.curRoll++] = pins }
+
+    frameSum(index) {return this.rolls[index] + this.rolls[index + 1] }
+
+    strikeBonus(index) { return this.rolls[index + 1] + this.rolls[index + 2] }
+
+    spareBonus(index) { return this.rolls[index + 2] }
+
+    isStrike(index) { return this.rolls[index] === 10 }
+    
+    isSpare(index) { return this.rolls[index] + this.rolls[index + 1] === 10 }
+
+    rollRounds(rounds, pins) { for (let i = 0; i < rounds; i++) this.roll(pins) }
+
+    rollStrike() { this.roll(10) }
+
+    rollSpare() {
+        this.roll(4)
+        this.roll(6)
     }
 }
 
--- tdd-bowling-kata/bowling-game.test.js
+++ tdd-bowling-kata/bowling-game.test.js
@@ -1,42 +1,36 @@
 const { BowlingGame }  = require('./bowling-game')
 
-const rollRounds = (rounds, pins) => { for (let i = 0; i < rounds; i++) bg.roll(pins) }
-
-const rollRounds = (pins) => { for (let i = 0; i < rounds; i++) bg.roll(pins) }
-
-
 describe('Bowling game tests', function() {
-    let bg = null
-
-    beforeAll(function() { bg = new BowlingGame() })
-
     test('Should return 0 points, when we have a gutter ball', function() {
-        rollRounds(20, 0)
-        expect(bg.score).toBe(0)
+        const bg = new BowlingGame()
+        bg.rollRounds(20, 0)
+        expect(bg.getScore()).toBe(0)
     })
 
     test('Should score 20 points, when we have only ones game', function() {
-        rollRounds(20, 1)
-        expect(bg.score).toBe(20)
+        const bg = new BowlingGame()
+        bg.rollRounds(20, 1)
+        expect(bg.getScore()).toBe(20)
     })
 
-    test('Should score 16 for a spare followed by a 3 points ball', () => {
-        rollSpare()
+    test('Should score 18 for a spare followed by a 4 points ball', function() {
+        const bg = new BowlingGame()
+        bg.rollSpare()
+        bg.roll(4)
+        expect(bg.getScore()).toBe(18)
+    })
+
+    test('Should score 28 for a strike followed by a 3 and a 6 points ball', function() {
+        const bg = new BowlingGame()
+        bg.rollStrike()
         bg.roll(3)
-        expect(bg.score()).toBe(16)
+        bg.roll(6)
+        expect(bg.getScore()).toBe(28)
     })
 
-    test('Should score 24 for a strike followed by a 3 and a 4 points ball', () => {
-        rollStrike()
-        g.roll(3)
-        g.roll(4)
-        rollMany(16, 0)
-        expect(g.score()).toBe(24)
+    test('Should score 300 for perfect game', function() {
+        const bg = new BowlingGame()
+        bg.rollRounds(12, 10)
+        expect(bg.getScore()).toBe(300)
     })
-
-    test('Should score 300 for perfect game', () => {
-        rollMany(12, 10)
-        expect(g.score()).toBe(300)
-    })
-
 })
